76ae9e5c9b0baf5eac8328e1c92f600f3d532bfc
Nabendu <nabendu.biswas@gmail.com>
2025-01-25 20:50:19 +0530

    First commit

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
+};
+
+export default nextConfig;
